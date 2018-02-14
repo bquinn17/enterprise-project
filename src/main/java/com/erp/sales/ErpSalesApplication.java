f25711096c85fbb1c47d01c7bf28d21537696a77
Bryan Quinn <blq9064@rit.edu>
2018-02-14 18:25:19 -0500

    Initial commit after generating SpringBoot application

---
+++ src/main/java/com/erp/sales/ErpSalesApplication.java
@@ -0,0 +1,12 @@
+package com.erp.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpSalesApplication.class, args);
+	}
+}
