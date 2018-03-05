019ec0325211d4ca692c790e50dc84e997b96ea5
peterhoward45 <peterhoward45>
2018-03-05 16:48:27 -0500

    Add check for at least one product in retail order

--- src/main/java/io/swagger/api/OrdersApiController.java
+++ src/main/java/io/swagger/api/OrdersApiController.java
@@ -29,8 +29,6 @@ public class OrdersApiController implements OrdersApi {
 
     public ResponseEntity<RetailOrder> addRetailOrder(@ApiParam(value = "Retail order object that needs to be added to the Sales System" ,required=true )  @Valid @RequestBody RetailOrder body) {
 
-
-
         // Create the Retail Order object with the info from body
         RetailOrder retailOrder = new RetailOrder();
         retailOrder.setCustomerEmail(body.getCustomerEmail());
@@ -41,6 +39,11 @@ public class OrdersApiController implements OrdersApi {
         retailOrder.setCustomerShippingZip(body.getCustomerShippingZip());
         retailOrder.setStatus(RetailOrder.StatusEnum.FULLFILLED);
         retailOrder.setProducts(body.getProducts());
+
+        // Check if the order contains at least one product.
+        if(body.getProducts().isEmpty()){
+            return new ResponseEntity<RetailOrder>(HttpStatus.BAD_REQUEST);
+        }
 
         // Save Object into database
         retailOrderRepository.save(retailOrder);
