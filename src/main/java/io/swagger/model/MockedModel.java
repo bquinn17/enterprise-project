0b430df7fb0c7e6b29738efdd70cf2222ca6c20c
BrendanJones44 <bpj1651@rit.edu>
2018-04-03 00:31:18 -0400

    add inventory mock

---
+++ src/main/java/io/swagger/model/MockedModel.java
@@ -0,0 +1,20 @@
+package io.swagger.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class MockedModel {
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("quantity")
+    private int quantity;
+
+    @JsonProperty("description")
+    private String description;
+
+    public MockedModel(String name, int quantity, String description){
+        this.name = name;
+        this.quantity = quantity;
+        this.description = description;
+    }
+}
