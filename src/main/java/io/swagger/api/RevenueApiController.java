4f8cd5959d1d2b534097caa8adc411b58303f0b0
Aaron Stadler <astadler777@gmail.com>
2018-04-19 14:06:38 -0400

    revenue endpoints without dates

--- src/main/java/io/swagger/api/RevenueApiController.java
+++ src/main/java/io/swagger/api/RevenueApiController.java
@@ -2,8 +2,9 @@ package io.swagger.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
+import io.swagger.model.RetailOrder;
 import io.swagger.model.WholesaleOrder;
-import io.swagger.repository.WholesaleAccountRepository;
+import io.swagger.repository.RetailOrderRepository;
 import io.swagger.repository.WholesaleOrderRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +12,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.*;
 import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Date;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-04-19T15:57:43.975Z")
 
 @Controller
@@ -25,16 +27,21 @@ public class RevenueApiController implements RevenueApi {
 
     private static final Logger log = LoggerFactory.getLogger(RevenueApiController.class);
 
+    private SimpleDateFormat dateFormat;
+
     private final ObjectMapper objectMapper;
 
     private final HttpServletRequest request;
 
     private WholesaleOrderRepository wholesaleOrderRepository;
 
+    private RetailOrderRepository retailOrderRepository;
+
     @org.springframework.beans.factory.annotation.Autowired
     public RevenueApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
+        this.dateFormat = new SimpleDateFormat("yyyy-dd-MM");
     }
 
     @CrossOrigin
@@ -44,11 +51,18 @@ public class RevenueApiController implements RevenueApi {
         List<WholesaleOrder> wholesaleOrders = wholesaleOrderRepository.findBySalesRepEmployeeId(Long.parseLong(sales_rep_id));
         double revenue = 0.0;
 
+        try{
+            Date to = dateFormat.parse(dateTo);
+            Date from = dateFormat.parse(dateFrom);
+        } catch (ParseException e) {
+            return new ResponseEntity<Double>(0.0, HttpStatus.BAD_REQUEST);
+        }
         for(WholesaleOrder order : wholesaleOrders) {
 
             revenue += order.getTotalPrice();
         }
-        return null;
+
+        return new ResponseEntity<Double>(revenue, HttpStatus.OK);
     }
 
     public ResponseEntity<Void> getRevenueFromRegion(@ApiParam(value = "",required=true) @PathVariable("region") String region) {
@@ -56,9 +70,29 @@ public class RevenueApiController implements RevenueApi {
         return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
     }
 
-    public ResponseEntity<Void> getTotalRevenue(@ApiParam(value = "") @Valid @RequestParam(value = "date_from", required = false) String dateFrom,@ApiParam(value = "") @Valid @RequestParam(value = "date_to", required = false) String dateTo) {
-        String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<Double> getTotalRevenue(@ApiParam(value = "") @Valid @RequestParam(value = "date_from", required = false) String dateFrom, @ApiParam(value = "") @Valid @RequestParam(value = "date_to", required = false) String dateTo) {
+        List<WholesaleOrder> wholesaleOrders = wholesaleOrderRepository.findAll();
+        List<RetailOrder> retailOrders = retailOrderRepository.findAll();
+
+        double revenue = 0.0;
+
+        try{
+            Date to = dateFormat.parse(dateTo);
+            Date from = dateFormat.parse(dateFrom);
+        } catch (ParseException e) {
+            return new ResponseEntity<Double>(0.0, HttpStatus.BAD_REQUEST);
+        }
+
+        for(WholesaleOrder order : wholesaleOrders) {
+
+            revenue += order.getTotalPrice();
+        }
+        for(RetailOrder order : retailOrders) {
+
+            revenue += order.getTotalPrice();
+        }
+
+        return new ResponseEntity<Double>(revenue, HttpStatus.OK);
     }
 
 }
